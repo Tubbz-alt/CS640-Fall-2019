84bb86c14d25165acda7493052772b284312c064
Shawn Zhong <public@shawnzhong.com>
2019-11-15 22:31:43 -0600

    Update L3Routing.java

--- Assignment4/src/edu/wisc/cs/sdn/apps/l3routing/L3Routing.java
+++ Assignment4/src/edu/wisc/cs/sdn/apps/l3routing/L3Routing.java
@@ -19,7 +19,9 @@ import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryListener;
 import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
 import net.floodlightcontroller.routing.Link;
 import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.action.OFAction;
 import org.openflow.protocol.action.OFActionOutput;
+import org.openflow.protocol.instruction.OFInstruction;
 import org.openflow.protocol.instruction.OFInstructionApplyActions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,11 +30,10 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class L3Routing implements IFloodlightModule, IOFSwitchListener,
-		ILinkDiscoveryListener, IDeviceListener
-{
-	public static final String MODULE_NAME = L3Routing.class.getSimpleName();
+        ILinkDiscoveryListener, IDeviceListener {
+    public static final String MODULE_NAME = L3Routing.class.getSimpleName();
 
-	// Interface to the logging system
+    // Interface to the logging system
     private static Logger log = LoggerFactory.getLogger(MODULE_NAME);
 
     // Interface to Floodlight core for interacting with connected switches
@@ -48,312 +49,299 @@ public class L3Routing implements IFloodlightModule, IOFSwitchListener,
     public static byte table;
 
     // Map of hosts to devices
-    private Map<IDevice,Host> knownHosts;
+    private Map<IDevice, Host> knownHosts;
 
     private Graph graph;
 
-	/**
+    /**
      * Loads dependencies and initializes data structures.
      */
-	@Override
-	public void init(FloodlightModuleContext context)
-			throws FloodlightModuleException
-	{
-		log.info(String.format("Initializing %s...", MODULE_NAME));
-		Map<String,String> config = context.getConfigParams(this);
+    @Override
+    public void init(FloodlightModuleContext context)
+            throws FloodlightModuleException {
+        log.info(String.format("Initializing %s...", MODULE_NAME));
+        Map<String, String> config = context.getConfigParams(this);
         table = Byte.parseByte(config.get("table"));
 
-		this.floodlightProv = context.getServiceImpl(
-				IFloodlightProviderService.class);
+        this.floodlightProv = context.getServiceImpl(
+                IFloodlightProviderService.class);
         this.linkDiscProv = context.getServiceImpl(ILinkDiscoveryService.class);
         this.deviceProv = context.getServiceImpl(IDeviceService.class);
 
-        this.knownHosts = new ConcurrentHashMap<IDevice,Host>();
-	}
+        this.knownHosts = new ConcurrentHashMap<IDevice, Host>();
+    }
 
-	/**
+    /**
      * Subscribes to events and performs other startup tasks.
      */
-	@Override
-	public void startUp(FloodlightModuleContext context)
-			throws FloodlightModuleException
-	{
-		log.info(String.format("Starting %s...", MODULE_NAME));
-		this.floodlightProv.addOFSwitchListener(this);
-		this.linkDiscProv.addListener(this);
-		this.deviceProv.addListener(this);
-		this.graph = new Graph(getSwitches().keySet());
-	}
+    @Override
+    public void startUp(FloodlightModuleContext context)
+            throws FloodlightModuleException {
+        log.info(String.format("Starting %s...", MODULE_NAME));
+        this.floodlightProv.addOFSwitchListener(this);
+        this.linkDiscProv.addListener(this);
+        this.deviceProv.addListener(this);
+        this.graph = new Graph(getSwitches().keySet());
+    }
 
-	public void installRules() {
-		for (Map.Entry<Long, Map<Long, Graph.LinkDistancePair>> entry : this.graph.getTable().entrySet()) {
-			long currentSwitchId = entry.getKey();
-			Map<Long, Graph.LinkDistancePair> linkMap = entry.getValue();
+    public void installRules() {
+        for (Map.Entry<Long, Map<Long, Graph.LinkDistancePair>> entry : this.graph.getTable().entrySet()) {
+            long currentSwitchId = entry.getKey();
+            Map<Long, Graph.LinkDistancePair> linkMap = entry.getValue();
 
-			IOFSwitch currentSwitch = this.floodlightProv.getSwitch(currentSwitchId);
+            IOFSwitch currentSwitch = this.floodlightProv.getSwitch(currentSwitchId);
 
-			for (Host host : getHosts()) {
-				long destinationSwitch = host.getSwitch().getId();
-				Link link = linkMap.get(destinationSwitch).link;
+            for (Host host : getHosts()) {
+                long destinationSwitch = host.getSwitch().getId();
+                Link link = linkMap.get(destinationSwitch).link;
 
-				OFMatch matchCriteria = new OFMatch();
-				matchCriteria.setDataLayerType(OFMatch.ETH_TYPE_IPV4);
-				matchCriteria.setNetworkDestination(host.getIPv4Address());
+                OFMatch matchCriteria = new OFMatch();
+                matchCriteria.setDataLayerType(OFMatch.ETH_TYPE_IPV4);
+                matchCriteria.setNetworkDestination(host.getIPv4Address());
 
-				OFActionOutput actionOutput = new OFActionOutput();
-				actionOutput.setPort(LinkUtils.getOtherPort(link, currentSwitchId));
+                OFActionOutput actionOutput = new OFActionOutput();
+                actionOutput.setPort(LinkUtils.getOtherPort(link, currentSwitchId));
 
-				OFInstructionApplyActions instruction = new OFInstructionApplyActions();
-				instruction.setActions(Arrays.asList(actionOutput));
+                OFInstructionApplyActions instruction = new OFInstructionApplyActions();
+                List<OFAction> actions = new ArrayList<OFAction>();
+                actions.add(actionOutput);
+                instruction.setActions(actions);
 
-				SwitchCommands.removeRules(currentSwitch, table, matchCriteria);
-				SwitchCommands.installRule(currentSwitch, table, SwitchCommands.DEFAULT_PRIORITY, matchCriteria, Arrays.asList(instruction));
-			}
-		}
-	}
+                SwitchCommands.removeRules(currentSwitch, table, matchCriteria);
+                List<OFInstruction> instructions = new ArrayList<OFInstruction>();
+                instructions.add(instruction);
+                SwitchCommands.installRule(currentSwitch, table, SwitchCommands.DEFAULT_PRIORITY, matchCriteria, instructions);
+            }
+        }
+    }
 
     /**
      * Get a list of all known hosts in the network.
      */
-    private Collection<Host> getHosts()
-    { return this.knownHosts.values(); }
+    private Collection<Host> getHosts() { return this.knownHosts.values(); }
 
     /**
      * Get a map of all active switches in the network. Switch DPID is used as
      * the key.
      */
-	private Map<Long, IOFSwitch> getSwitches()
-    { return floodlightProv.getAllSwitchMap(); }
+    private Map<Long, IOFSwitch> getSwitches() { return floodlightProv.getAllSwitchMap(); }
 
     /**
      * Get a list of all active links in the network.
      */
-    private Collection<Link> getLinks()
-    { return linkDiscProv.getLinks().keySet(); }
+    private Collection<Link> getLinks() { return linkDiscProv.getLinks().keySet(); }
 
     /**
      * Event handler called when a host joins the network.
+     *
      * @param device information about the host
      */
-	@Override
-	public void deviceAdded(IDevice device)
-	{
-		Host host = new Host(device, this.floodlightProv);
-		// We only care about a new host if we know its IP
-		if (host.getIPv4Address() != null)
-		{
-			log.info(String.format("Host %s added", host.getName()));
-			this.knownHosts.put(device, host);
+    @Override
+    public void deviceAdded(IDevice device) {
+        Host host = new Host(device, this.floodlightProv);
+        // We only care about a new host if we know its IP
+        if (host.getIPv4Address() != null) {
+            log.info(String.format("Host %s added", host.getName()));
+            this.knownHosts.put(device, host);
 
-			installRules();
-		}
-	}
+            installRules();
+        }
+    }
 
-	/**
+    /**
      * Event handler called when a host is no longer attached to a switch.
+     *
      * @param device information about the host
      */
-	@Override
-	public void deviceRemoved(IDevice device)
-	{
-		Host host = this.knownHosts.get(device);
-		if (null == host)
-		{ return; }
-		this.knownHosts.remove(device);
+    @Override
+    public void deviceRemoved(IDevice device) {
+        Host host = this.knownHosts.get(device);
+        if (null == host) { return; }
+        this.knownHosts.remove(device);
 
-		log.info(String.format("Host %s is no longer attached to a switch",
-				host.getName()));
+        log.info(String.format("Host %s is no longer attached to a switch",
+                host.getName()));
 
-		installRules();
-	}
+        installRules();
+    }
 
-	/**
+    /**
      * Event handler called when a host moves within the network.
+     *
      * @param device information about the host
      */
-	@Override
-	public void deviceMoved(IDevice device)
-	{
-		Host host = this.knownHosts.get(device);
-		if (null == host)
-		{
-			host = new Host(device, this.floodlightProv);
-			this.knownHosts.put(device, host);
-		}
+    @Override
+    public void deviceMoved(IDevice device) {
+        Host host = this.knownHosts.get(device);
+        if (null == host) {
+            host = new Host(device, this.floodlightProv);
+            this.knownHosts.put(device, host);
+        }
 
-		if (!host.isAttachedToSwitch())
-		{
-			this.deviceRemoved(device);
-			return;
-		}
-		log.info(String.format("Host %s moved to s%d:%d", host.getName(),
-				host.getSwitch().getId(), host.getPort()));
+        if (!host.isAttachedToSwitch()) {
+            this.deviceRemoved(device);
+            return;
+        }
+        log.info(String.format("Host %s moved to s%d:%d", host.getName(),
+                host.getSwitch().getId(), host.getPort()));
 
-		installRules();
-	}
+        installRules();
+    }
 
     /**
      * Event handler called when a switch joins the network.
+     *
      * @param DPID for the switch
      */
-	@Override
-	public void switchAdded(long switchId)
-	{
-		IOFSwitch sw = this.floodlightProv.getSwitch(switchId);
-		log.info(String.format("Switch s%d added", switchId));
+    @Override
+    public void switchAdded(long switchId) {
+        IOFSwitch sw = this.floodlightProv.getSwitch(switchId);
+        log.info(String.format("Switch s%d added", switchId));
 
-		this.graph.addSwitch(switchId);
-		this.graph.updateTable(getLinks());
-		installRules();
-	}
+        this.graph.addSwitch(switchId);
+        this.graph.updateTable(getLinks());
+        installRules();
+    }
 
-	/**
-	 * Event handler called when a switch leaves the network.
-	 * @param DPID for the switch
-	 */
-	@Override
-	public void switchRemoved(long switchId)
-	{
-		IOFSwitch sw = this.floodlightProv.getSwitch(switchId);
-		log.info(String.format("Switch s%d removed", switchId));
+    /**
+     * Event handler called when a switch leaves the network.
+     *
+     * @param DPID for the switch
+     */
+    @Override
+    public void switchRemoved(long switchId) {
+        IOFSwitch sw = this.floodlightProv.getSwitch(switchId);
+        log.info(String.format("Switch s%d removed", switchId));
 
-		this.graph.removeSwitch(switchId);
-		this.graph.recomputeTable(getLinks());
-		installRules();
-	}
+        this.graph.removeSwitch(switchId);
+        this.graph.recomputeTable(getLinks());
+        installRules();
+    }
 
-	/**
-	 * Event handler called when multiple links go up or down.
-	 * @param updateList information about the change in each link's state
-	 */
-	@Override
-	public void linkDiscoveryUpdate(List<LDUpdate> updateList)
-	{
-		for (LDUpdate update : updateList)
-		{
-			// If we only know the switch & port for one end of the link, then
-			// the link must be from a switch to a host
-			if (0 == update.getDst())
-			{
-				log.info(String.format("Link s%s:%d -> host updated",
-					update.getSrc(), update.getSrcPort()));
-			}
-			// Otherwise, the link is between two switches
-			else
-			{
-				log.info(String.format("Link s%s:%d -> s%s:%d updated",
-					update.getSrc(), update.getSrcPort(),
-					update.getDst(), update.getDstPort()));
-			}
-		}
+    /**
+     * Event handler called when multiple links go up or down.
+     *
+     * @param updateList information about the change in each link's state
+     */
+    @Override
+    public void linkDiscoveryUpdate(List<LDUpdate> updateList) {
+        for (LDUpdate update : updateList) {
+            // If we only know the switch & port for one end of the link, then
+            // the link must be from a switch to a host
+            if (0 == update.getDst()) {
+                log.info(String.format("Link s%s:%d -> host updated",
+                        update.getSrc(), update.getSrcPort()));
+            }
+            // Otherwise, the link is between two switches
+            else {
+                log.info(String.format("Link s%s:%d -> s%s:%d updated",
+                        update.getSrc(), update.getSrcPort(),
+                        update.getDst(), update.getDstPort()));
+            }
+        }
 
-		this.graph.recomputeTable(getLinks());
-		installRules();
-	}
+        this.graph.recomputeTable(getLinks());
+        installRules();
+    }
 
-	/**
-	 * Event handler called when link goes up or down.
-	 * @param update information about the change in link state
-	 */
-	@Override
-	public void linkDiscoveryUpdate(LDUpdate update)
-	{ this.linkDiscoveryUpdate(Arrays.asList(update)); }
+    /**
+     * Event handler called when link goes up or down.
+     *
+     * @param update information about the change in link state
+     */
+    @Override
+    public void linkDiscoveryUpdate(LDUpdate update) { this.linkDiscoveryUpdate(Arrays.asList(update)); }
 
-	/**
+    /**
      * Event handler called when the IP address of a host changes.
+     *
      * @param device information about the host
      */
-	@Override
-	public void deviceIPV4AddrChanged(IDevice device)
-	{ this.deviceAdded(device); }
+    @Override
+    public void deviceIPV4AddrChanged(IDevice device) { this.deviceAdded(device); }
 
-	/**
+    /**
      * Event handler called when the VLAN of a host changes.
+     *
      * @param device information about the host
      */
-	@Override
-	public void deviceVlanChanged(IDevice device)
-	{ /* Nothing we need to do, since we're not using VLANs */ }
+    @Override
+    public void deviceVlanChanged(IDevice device) { /* Nothing we need to do, since we're not using VLANs */ }
 
-	/**
-	 * Event handler called when the controller becomes the master for a switch.
-	 * @param DPID for the switch
-	 */
-	@Override
-	public void switchActivated(long switchId)
-	{ /* Nothing we need to do, since we're not switching controller roles */ }
+    /**
+     * Event handler called when the controller becomes the master for a switch.
+     *
+     * @param DPID for the switch
+     */
+    @Override
+    public void switchActivated(long switchId) { /* Nothing we need to do, since we're not switching controller roles */ }
 
-	/**
-	 * Event handler called when some attribute of a switch changes.
-	 * @param DPID for the switch
-	 */
-	@Override
-	public void switchChanged(long switchId)
-	{ /* Nothing we need to do */ }
+    /**
+     * Event handler called when some attribute of a switch changes.
+     *
+     * @param DPID for the switch
+     */
+    @Override
+    public void switchChanged(long switchId) { /* Nothing we need to do */ }
 
-	/**
-	 * Event handler called when a port on a switch goes up or down, or is
-	 * added or removed.
-	 * @param DPID for the switch
-	 * @param port the port on the switch whose status changed
-	 * @param type the type of status change (up, down, add, remove)
-	 */
-	@Override
-	public void switchPortChanged(long switchId, ImmutablePort port,
-			PortChangeType type)
-	{ /* Nothing we need to do, since we'll get a linkDiscoveryUpdate event */ }
+    /**
+     * Event handler called when a port on a switch goes up or down, or is
+     * added or removed.
+     *
+     * @param DPID for the switch
+     * @param port the port on the switch whose status changed
+     * @param type the type of status change (up, down, add, remove)
+     */
+    @Override
+    public void switchPortChanged(long switchId, ImmutablePort port,
+                                  PortChangeType type) { /* Nothing we need to do, since we'll get a linkDiscoveryUpdate event */ }
 
-	/**
-	 * Gets a name for this module.
-	 * @return name for this module
-	 */
-	@Override
-	public String getName()
-	{ return this.MODULE_NAME; }
+    /**
+     * Gets a name for this module.
+     *
+     * @return name for this module
+     */
+    @Override
+    public String getName() { return this.MODULE_NAME; }
 
-	/**
-	 * Check if events must be passed to another module before this module is
-	 * notified of the event.
-	 */
-	@Override
-	public boolean isCallbackOrderingPrereq(String type, String name)
-	{ return false; }
+    /**
+     * Check if events must be passed to another module before this module is
+     * notified of the event.
+     */
+    @Override
+    public boolean isCallbackOrderingPrereq(String type, String name) { return false; }
 
-	/**
-	 * Check if events must be passed to another module after this module has
-	 * been notified of the event.
-	 */
-	@Override
-	public boolean isCallbackOrderingPostreq(String type, String name)
-	{ return false; }
+    /**
+     * Check if events must be passed to another module after this module has
+     * been notified of the event.
+     */
+    @Override
+    public boolean isCallbackOrderingPostreq(String type, String name) { return false; }
 
     /**
      * Tell the module system which services we provide.
      */
-	@Override
-	public Collection<Class<? extends IFloodlightService>> getModuleServices()
-	{ return null; }
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleServices() { return null; }
 
-	/**
+    /**
      * Tell the module system which services we implement.
      */
-	@Override
-	public Map<Class<? extends IFloodlightService>, IFloodlightService>
-			getServiceImpls()
-	{ return null; }
+    @Override
+    public Map<Class<? extends IFloodlightService>, IFloodlightService>
+    getServiceImpls() { return null; }
 
-	/**
+    /**
      * Tell the module system which modules we depend on.
      */
-	@Override
-	public Collection<Class<? extends IFloodlightService>>
-			getModuleDependencies()
-	{
-		Collection<Class<? extends IFloodlightService >> floodlightService =
-	            new ArrayList<Class<? extends IFloodlightService>>();
+    @Override
+    public Collection<Class<? extends IFloodlightService>>
+    getModuleDependencies() {
+        Collection<Class<? extends IFloodlightService>> floodlightService =
+                new ArrayList<Class<? extends IFloodlightService>>();
         floodlightService.add(IFloodlightProviderService.class);
         floodlightService.add(ILinkDiscoveryService.class);
         floodlightService.add(IDeviceService.class);
         return floodlightService;
-	}
+    }
 }
