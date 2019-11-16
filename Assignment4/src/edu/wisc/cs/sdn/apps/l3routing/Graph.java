e079a01292a31ae9654d3611136e6b0865b69a37
Shawn Zhong <public@shawnzhong.com>
2019-11-15 18:59:15 -0600

    Add installRules

---
+++ Assignment4/src/edu/wisc/cs/sdn/apps/l3routing/Graph.java
@@ -0,0 +1,32 @@
+package edu.wisc.cs.sdn.apps.l3routing;
+
+import net.floodlightcontroller.routing.Link;
+
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+    private Map<Long, List<Link>> graph;
+
+    Map<Long, Map<Long, Link>> table;
+
+    private void updateTable() {
+        return;
+    }
+
+    private void addSwitch(Long switchId){
+
+    }
+
+    private void removeSwitch(Long switchId){
+
+    }
+
+    private void addLink(Link link){
+
+    }
+
+    private void removeLink(Link link){
+
+    }
+}
